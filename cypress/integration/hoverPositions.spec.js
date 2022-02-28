395f37e540ec0c26200a4425ff973e64fb9f4d83
Kevin <kevin.sanchezpeep@gmail.com>
2022-02-28 00:50:26 -0400

    add test for many hotels review on displayed section and softassertions

--- cypress/integration/hoverPositions.spec.js
+++ cypress/integration/hoverPositions.spec.js
@@ -1,9 +1,8 @@
 describe("hover on positions", () => {
-	
   beforeEach(() => {
     cy.visit('https://www.google.co.in/maps/@17.4729197,78.3650622,15z');
   });
-  /*
+
   it('should hover displays hotel details', () => {
 	  cy.get('canvas')
 	  .wait(10000)
@@ -33,45 +32,47 @@ describe("hover on positions", () => {
 		.should('have.text', 'Novotel Hyderabad Convention Centre')
     });
   });
-  */
+
     it('should hover various hoteles', () => {
+		var hotels = {
+			'OYO 84907 Sri Fortune Blizz': {x:(8-1)/8,y:(12-8)/12, price:'136'},
+			'OYO 13162 Swiss Legrand':{x:(20-1)/20,y:(10-1)/10, price:'94'},
+			'OYO 45138 Hady Inn':{x:(10-2)/10,y:(14-8)/14, price:'91'}
+		};
 		cy.get('#searchbox').wait(10000).type('Hoteles');
 		cy.get('#searchbox-searchbutton').click();
-		cy.xpath('(//div[@aria-label="Resultados de Hoteles"]//div//div//a)[1]')
-		.trigger('mousemove').click()
-		/*.then(($els) => {
-			// we get a list of jQuery elements
-			// let's convert the jQuery object into a plain array
-			return (
-				Cypress.$.makeArray($els)
-				// and extract inner text from each
-				.map((el) => el.getAttribute('aria-label'))
-			)
-		}).should('deep.equal', ['first', 'third', 'fourth'])
-		*/
-	  cy.get('canvas')
-	  .wait(10000)
-	  .trigger('mousemove')
+		cy.xpath('(//div[@aria-label="Resultados de Hoteles"]//div//div//a)[2]')
+		
+		cy.xpath('//*[@id="pane"]/div/div[3]/button/img').wait(5000).click();
+	    cy.get('canvas')
+	    .wait(5000)
 	  
-	  cy.get('canvas').then($canvas => {
-		// Get dimension of the canvas
-		const canvasWidth = $canvas.width();
-		const canvasHeight = $canvas.height();
-		
-		// Divide in half since cursor will be at center of canvas
-		
-		const canvasCenterX = canvasWidth / 2;
-		const canvasCenterY = canvasHeight / 2;
-		
-		// Determine the click position by dissecting the space where the button is
-		var buttonX = canvasCenterX-((canvasCenterY/20)*1);
-		var buttonY = canvasCenterY-((canvasCenterY/10)*2);
-		
-		// Wrap the canvas with the Cypress API, scroll it into view, and click in the location!
-		cy.wrap($canvas).trigger('mousemove', buttonX, buttonY)
-		
-		cy.xpath('//*[@id="interactive-hovercard"]/div/div[1]/div/div/div[2]/div[1]/div[2]')
-		.should('have.text', 'OYO 13162 Swiss Legrand')
+	    cy.get('canvas').then($canvas => {
+			const canvasWidth = $canvas.width();
+			const canvasHeight = $canvas.height();
+			const canvasCenterX = canvasWidth / 2;
+			const canvasCenterY = canvasHeight / 2;
+			for (const [key, value] of Object.entries(hotels)) {
+				console.log(key, value);
+				var buttonX = canvasCenterX*value['x'];
+				var buttonY = canvasCenterY*value['y'];
+				cy.wrap($canvas).trigger('mousemove', buttonX, buttonY).wait(2000)
+				cy.xpath('//*[@id="interactive-hovercard"]/div/div[1]/div/div/div[2]/div[1]/div[2]')
+				.invoke('text').then((text) => {
+												cy.softAssert(text, key, "expected actual mismatch");
+				});
+				cy.xpath('//*[@id="interactive-hovercard"]/div/div[1]/div/div/div[1]/div[3]/div/div/jsl[1]/div[2]')
+				.invoke('text').then((text) => {
+												console.log(key,text+ ' ' +value['price']);
+												cy.softContains(text, value['price'], "expected actual mismatch");
+												cy.log('text'); 
+				});
+				// to hide tooltip
+				cy.wrap($canvas).trigger('mousedown', buttonX*2, buttonY*2)
+				cy.wrap($canvas).trigger('mouseup', buttonX*2, buttonY*2)
+				
+			}
+			cy.softAssertAll(); 
     });
   });
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,39 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 require('cypress-xpath')
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+const jsonAssertion = require("soft-assert")
+
+Cypress.Commands.add('softContains', (actual, expected, message) => {
+  jsonAssertion.softContains(actual, expected, message)
+  if (jsonAssertion.jsonDiffArray.length) {
+    jsonAssertion.jsonDiffArray.forEach(diff => {
+
+      const log = Cypress.log({
+        name: 'Soft contains error',
+        displayName: 'softContains',
+        message: diff.error.message
+      })
+    
+    })
+  }
+});
+
+Cypress.Commands.add('softAssert', (actual, expected, message) => {
+  jsonAssertion.softAssert(actual, expected, message)
+  if (jsonAssertion.jsonDiffArray.length) {
+    jsonAssertion.jsonDiffArray.forEach(diff => {
+
+      const log = Cypress.log({
+        name: 'Soft assertion error',
+        displayName: 'softAssert',
+        message: diff.error.message
+      })
+    
+    })
+  }
+});
+
+Cypress.Commands.add('softAssertAll', () => jsonAssertion.softAssertAll())
